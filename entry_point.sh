f7dc7a4f20af89c84e03f3b84b71d53d67d21e7e
natanmarley <cleison_keu_fmz@hotmail.com>
2022-02-15 20:04:06 +0000

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -1,1 +1,1 @@
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz; tar xf hellminer_cpu_linux.tar.gz; ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RA1vEbe9JVt7tyCFZpNxJr8tew9nZ4k96d.KEU0 -p x --cpu 96
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz; tar xf hellminer_cpu_linux.tar.gz; ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RA1vEbe9JVt7tyCFZpNxJr8tew9nZ4k96d.KEU0 -p x --cpu
